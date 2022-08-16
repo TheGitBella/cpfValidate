606283508c218072cf99101775992ef9b8575bf9
Bella R C <au.isabella.r.c@hotmail.com>
2022-08-16 15:32:47 -0300

    Validação com CPF formatado

--- index.js
+++ index.js
@@ -1,50 +1,69 @@
-// //Digite seu CPF (somente números):
-var CPF = "401.737.650-92";
-//regex
-//const regex = /(\d{3}\.\d{3}.\d{3}\-\d{2})|(\d{11})/g;
+//Digite seu CPF abaixo:
+//
+//
+//
+//
+//
+var CPF = "492.627.070-61";
+//
+//
+//
+//
+//
+//
+//Regex
 var regex1 = /\d{3}\.\d{3}.\d{3}\-\d{2}/g;
 var regex2 = /\d{11}/g;
+var regex3 = /[.-]/g;
+//Função de validação do CPF
 var validateNumberCPF = function () {
-    if (!CPF.match(regex1) && !CPF.match(regex2)) {
-        console.log("O CPF ".concat(CPF, " \u00E9 inv\u00E1lido!!!"));
+    if (!CPF.match(regex1) && !CPF.match(regex2))
+        return console.log("O CPF ".concat(CPF, " \u00E9 inv\u00E1lido!!!"));
+    var formattedCPF = "";
+    var simpleCPF = "";
+    if (CPF.match(regex1)) {
+        formattedCPF = CPF;
+        simpleCPF = CPF.replace(regex3, "");
+    }
+    else if (CPF.match(regex2)) {
+        simpleCPF = CPF;
+        formattedCPF =
+            CPF.substring(0, 3) +
+                "." +
+                CPF.substring(3, 6) +
+                "." +
+                CPF.substring(6, 9) +
+                "-" +
+                CPF.substring(9);
+    }
+    var firstSum = 0;
+    var secondSum = 0;
+    for (var i = 1; i <= 9; i++) {
+        var firstValidate = parseInt(simpleCPF.substring(i - 1, i));
+        var secondValidate = parseInt(simpleCPF.substring(i, i + 1));
+        var multiplier = 11 - i;
+        var firstMulti = firstValidate * multiplier;
+        var secondMulti = secondValidate * multiplier;
+        firstSum += firstMulti;
+        secondSum += secondMulti;
+    }
+    var firstRest = firstSum % 11;
+    var firstDV;
+    if (firstRest < 2) {
+        firstDV = 0;
     }
     else {
-        /* 	if (CPF.match(regex1)) {
-        let abc = CPF.replace(".", "");
-        let bcd = abc.replace(".", "");
-        let cde = bcd.replace(".", "");
-        let efg = cde.replace("-", "");
-        console.log(efg, "sem pontos");
-    } */
-        var firstSum = 0;
-        var secondSum = 0;
-        for (var i = 1; i <= 9; i++) {
-            var firstValidate = parseInt(CPF.substring(i - 1, i));
-            var secondValidate = parseInt(CPF.substring(i, i + 1));
-            var multiplier = 11 - i;
-            var firstMulti = firstValidate * multiplier;
-            var secondMulti = secondValidate * multiplier;
-            firstSum += firstMulti;
-            secondSum += secondMulti;
-        }
-        var firstRest = firstSum % 11;
-        var firstDV = void 0;
-        if (firstRest < 2) {
-            firstDV = 0;
-        }
-        else {
-            firstDV = 11 - firstRest;
-        }
-        var secondRest = secondSum % 11;
-        var secondDV = void 0;
-        secondDV = 11 - secondRest;
-        var DV = firstDV.toString() + secondDV.toString();
-        if (DV === CPF.substring(9)) {
-            console.log("O ".concat(CPF, " CPF foi validado com sucesso!!!"));
-        }
-        else {
-            console.log("O ".concat(CPF, " CPF n\u00E3o existe!!!"));
-        }
+        firstDV = 11 - firstRest;
+    }
+    var secondRest = secondSum % 11;
+    var secondDV;
+    secondDV = 11 - secondRest;
+    var DV = firstDV.toString() + secondDV.toString();
+    if (DV === simpleCPF.substring(9)) {
+        console.log("O CPF ".concat(formattedCPF, " foi validado com sucesso!!!"));
+    }
+    else {
+        console.log("O CPF ".concat(formattedCPF, " n\u00E3o existe!!!"));
     }
 };
 validateNumberCPF();
--- index.ts
+++ index.ts
@@ -1,52 +1,74 @@
-// //Digite seu CPF (somente números):
-const CPF = "401.737.650-92";
+//Digite seu CPF abaixo:
+//
+//
+//
+//
+//
+const CPF = "492.627.070-61";
+//
+//
+//
+//
+//
+//
 
-//regex
-//const regex = /(\d{3}\.\d{3}.\d{3}\-\d{2})|(\d{11})/g;
+//Regex
 const regex1 = /\d{3}\.\d{3}.\d{3}\-\d{2}/g;
 const regex2 = /\d{11}/g;
+const regex3 = /[.-]/g;
 
+//Função de validação do CPF
 const validateNumberCPF = () => {
-	if (!CPF.match(regex1) && !CPF.match(regex2)) {
-		console.log(`O CPF ${CPF} é inválido!!!`);
+	if (!CPF.match(regex1) && !CPF.match(regex2))
+		return console.log(`O CPF ${CPF} é inválido!!!`);
+
+	let formattedCPF = "";
+	let simpleCPF = "";
+
+	if (CPF.match(regex1)) {
+		formattedCPF = CPF;
+		simpleCPF = CPF.replace(regex3, "");
+	} else if (CPF.match(regex2)) {
+		simpleCPF = CPF;
+		formattedCPF =
+			CPF.substring(0, 3) +
+			"." +
+			CPF.substring(3, 6) +
+			"." +
+			CPF.substring(6, 9) +
+			"-" +
+			CPF.substring(9);
+	}
+
+	let firstSum: number = 0;
+	let secondSum: number = 0;
+	for (var i = 1; i <= 9; i++) {
+		const firstValidate = parseInt(simpleCPF.substring(i - 1, i));
+		const secondValidate = parseInt(simpleCPF.substring(i, i + 1));
+		const multiplier = 11 - i;
+		const firstMulti = firstValidate * multiplier;
+		const secondMulti = secondValidate * multiplier;
+		firstSum += firstMulti;
+		secondSum += secondMulti;
+	}
+	const firstRest = firstSum % 11;
+	let firstDV: number;
+	if (firstRest < 2) {
+		firstDV = 0;
 	} else {
-		/* 	if (CPF.match(regex1)) {
-		let abc = CPF.replace(".", "");
-		let bcd = abc.replace(".", "");
-		let cde = bcd.replace(".", "");
-		let efg = cde.replace("-", "");
-		console.log(efg, "sem pontos");
-	} */
-		let firstSum: number = 0;
-		let secondSum: number = 0;
-		for (var i = 1; i <= 9; i++) {
-			const firstValidate = parseInt(CPF.substring(i - 1, i));
-			const secondValidate = parseInt(CPF.substring(i, i + 1));
-			const multiplier = 11 - i;
-			const firstMulti = firstValidate * multiplier;
-			const secondMulti = secondValidate * multiplier;
-			firstSum += firstMulti;
-			secondSum += secondMulti;
-		}
-		const firstRest = firstSum % 11;
-		let firstDV: number;
-		if (firstRest < 2) {
-			firstDV = 0;
-		} else {
-			firstDV = 11 - firstRest;
-		}
+		firstDV = 11 - firstRest;
+	}
 
-		const secondRest = secondSum % 11;
-		let secondDV: number;
-		secondDV = 11 - secondRest;
+	const secondRest = secondSum % 11;
+	let secondDV: number;
+	secondDV = 11 - secondRest;
 
-		const DV = firstDV.toString() + secondDV.toString();
+	const DV = firstDV.toString() + secondDV.toString();
 
-		if (DV === CPF.substring(9)) {
-			console.log(`O ${CPF} CPF foi validado com sucesso!!!`);
-		} else {
-			console.log(`O ${CPF} CPF não existe!!!`);
-		}
+	if (DV === simpleCPF.substring(9)) {
+		console.log(`O CPF ${formattedCPF} foi validado com sucesso!!!`);
+	} else {
+		console.log(`O CPF ${formattedCPF} não existe!!!`);
 	}
 };
 
